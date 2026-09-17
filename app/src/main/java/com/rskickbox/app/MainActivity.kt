40d39d86d327f6863c9d0f704a3fb3080b634433
eucridernlpro <eucridernl@gmail.com>
2026-09-17 23:34:17 +0200

    Switch app entry point to v0.18 premium dashboard

--- app/src/main/java/com/rskickbox/app/MainActivity.kt
+++ app/src/main/java/com/rskickbox/app/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { RsKickboxV17App() }
+        setContent { RsKickboxV18App() }
     }
 }
