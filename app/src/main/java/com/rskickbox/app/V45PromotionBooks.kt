8013954b7c1f2f88d564163ac0d3d16ea00c50ba
eucridernlpro <eucridernl@gmail.com>
2026-09-18 20:08:05 +0200

    Fix book reader Alignment import

--- app/src/main/java/com/rskickbox/app/V45PromotionBooks.kt
+++ app/src/main/java/com/rskickbox/app/V45PromotionBooks.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
