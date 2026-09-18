eb851643aa53256262a53f091f4560d488dead9e
eucridernlpro <eucridernl@gmail.com>
2026-09-18 21:28:37 +0200

    Fix Release Center Supabase probe text unit import

--- app/src/main/java/com/rskickbox/app/V16ProductionPreview.kt
+++ app/src/main/java/com/rskickbox/app/V16ProductionPreview.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 private fun RsStatusBannerV16(c: RsPalette, message: String) {
