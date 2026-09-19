27fc3e3683a9a58cfcabfcdcf40e1c13ae36897d
eucridernlpro <eucridernl@gmail.com>
2026-09-19 21:50:59 +0200

    Advance app version to v0.112.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 111
-        versionName = "0.111.0"
+        versionCode = 112
+        versionName = "0.112.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
