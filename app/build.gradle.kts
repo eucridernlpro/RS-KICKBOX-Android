98470b7788dffb9dc311a4e6ca49247ba61da052
eucridernlpro <eucridernl@gmail.com>
2026-09-19 11:41:16 +0200

    Add Media3 session support for persistent music

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.media3:media3-effect:1.11.1")
     implementation("androidx.media3:media3-common:1.11.1")
     implementation("androidx.media3:media3-exoplayer:1.11.1")
+    implementation("androidx.media3:media3-session:1.11.1")
     implementation("androidx.media3:media3-ui:1.11.1")
     implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
     implementation("com.google.zxing:core:3.5.4")
