1c7fc1690a4e4db1e5595b183f71ab078915c15d
eucridernlpro <eucridernl@gmail.com>
2026-09-19 13:34:59 +0200

    Bump cloud intro milestone to v0.102

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 101
-        versionName = "0.101.0"
+        versionCode = 102
+        versionName = "0.102.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
