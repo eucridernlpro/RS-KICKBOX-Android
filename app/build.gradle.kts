d7a3b6eaeaf1ec648ad9d28edf6172719484136b
eucridernlpro <eucridernl@gmail.com>
2026-09-19 08:30:23 +0200

    Bump cloud events milestone to v0.75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.74.0"
+        versionCode = 75
+        versionName = "0.75.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
