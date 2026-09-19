f9c90a198ef9a0363928a9c059d060f7b425b1f4
eucridernlpro <eucridernl@gmail.com>
2026-09-19 10:18:21 +0200

    Bump refreshed login test build to v0.88

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 87
-        versionName = "0.87.0"
+        versionCode = 88
+        versionName = "0.88.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
