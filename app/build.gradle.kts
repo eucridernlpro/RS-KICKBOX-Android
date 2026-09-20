edabc4d39711e187bb783e42e6d8bdb8a17dea82
eucridernlpro <eucridernl@gmail.com>
2026-09-20 10:26:09 +0200

    Package v0.116.0 chat and visual repair build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 116
-        versionName = "0.115.0"
+        versionCode = 117
+        versionName = "0.116.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
