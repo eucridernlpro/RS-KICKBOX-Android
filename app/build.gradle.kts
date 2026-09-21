9075f546224472e85afbeb6032994e887c718eb4
eucridernlpro <eucridernl@gmail.com>
2026-09-21 18:53:43 +0200

    Bump preview to v0.153.0 for communications upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 153
-        versionName = "0.152.0"
+        versionCode = 154
+        versionName = "0.153.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
