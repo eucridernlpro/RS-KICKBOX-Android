35584d8b52dd9909362043e90c989c7299eef954
eucridernlpro <eucridernl@gmail.com>
2026-09-21 15:05:36 +0200

    Bump preview to v0.149.0 for call quality and splash preview fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.rskickbox.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 149
-        versionName = "0.148.0"
+        versionCode = 150
+        versionName = "0.149.0"
         buildConfigField("String", "SUPABASE_URL", "\""+rsSupabaseUrl.replace("\\","\\\\").replace("\"","\\\"")+"\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\""+rsSupabasePublishableKey.replace("\\","\\\\").replace("\"","\\\"")+"\"")
     }
