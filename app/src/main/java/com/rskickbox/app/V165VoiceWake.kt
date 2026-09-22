042be0ce21cac94d34cd2f5ea714911ad31f99de
eucridernlpro <eucridernl@gmail.com>
2026-09-23 00:47:24 +0200

    Fix Uri import for voice playlist playback

--- app/src/main/java/com/rskickbox/app/V165VoiceWake.kt
+++ app/src/main/java/com/rskickbox/app/V165VoiceWake.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.AudioManager
+import android.net.Uri
 import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
