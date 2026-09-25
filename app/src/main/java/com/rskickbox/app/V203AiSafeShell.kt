3cc6bf96e3ec18386b6a92d498bc52e2e0775de9
eucridernlpro <eucridernl@gmail.com>
2026-09-26 01:47:33 +0200

    Fix AI safe shell horizontal language scroller import

--- app/src/main/java/com/rskickbox/app/V203AiSafeShell.kt
+++ app/src/main/java/com/rskickbox/app/V203AiSafeShell.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
